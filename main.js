0eb3b8c06ea083009498cc975d10fa98600d68bd
7220 <kumasaki@naver.com>
2023-04-15 00:18:39 +0900

    function add

--- main.js
+++ main.js
@@ -6,20 +6,71 @@ const compression = require("compression");
 const topicRouter = require("./routes/topic.js");
 const indexRouter = require("./routes/index.js");
 const authRouter = require("./routes/auth.js");
-const helmet = require('helmet')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
-app.use(helmet())
+const helmet = require("helmet");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+app.use(helmet());
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.use(session({
-  secret: 'snelfknae@#!@3',
-  resave: false,
-  saveUninitialized: true,
-  store: new FileStore()
-}))
+app.use(
+  session({
+    secret: "snelfknae@#!@3",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore(),
+  })
+);
+
+var authData = {
+  email: "asd@asd.com",
+  password: "123123",
+  nickname: "dsa",
+};
+
+const passport = require("passport");
+const localStrategy = require("passport-local");
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(function(user, done) {
+  console.log('serial : ', user)
+})
+
+passport.deserializeUser(function(id, done) {
+  console.log('id : ', id)
+})
+
+passport.use(
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "pwd",
+    },
+    function (username, password, done) {
+      console.log("localStrategy", username, password);
+      if (username === authData.email) {
+        if (password === authData.password) {
+          return done(null, authData);
+        } else {
+          return done(null, false, { message: "Incorrect password" });
+        }
+      } else {
+        return done(null, false, { message: "Incorrect username" });
+      }
+    }
+  )
+);
+
+app.post(
+  "/auth/login_process",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+  })
+);
 
 app.get("*", (request, response, next) => {
   fs.readdir("./data", function (error, filelist) {
--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,6 @@ const fs = require("fs");
 const sanitizeHtml = require("sanitize-html");
 const template = require("../lib/template.js");
 
-var authData = {
-  email: "asd@asd.com",
-  password: "123123",
-  nickname: "dsa",
-};
-
 router.get("/login", (request, response) => {
   var title = "WEB - login";
   var list = template.list(request.list);
@@ -29,20 +23,20 @@ router.get("/login", (request, response) => {
   response.send(html);
 });
 
-router.post("/login_process", (request, response) => {
-  var post = request.body;
-  var email = post.email;
-  var password = post.pwd;
-  if (email === authData.email && password === authData.password) {
-    request.session.is_logined = true;
-    request.session.nickname = authData.nickname;
-    request.session.save(() => {
-      response.redirect(`/`);
-    });
-  } else {
-    response.send("??");
-  }
-});
+// router.post("/login_process", (request, response) => {
+//   var post = request.body;
+//   var email = post.email;
+//   var password = post.pwd;
+//   if (email === authData.email && password === authData.password) {
+//     request.session.is_logined = true;
+//     request.session.nickname = authData.nickname;
+//     request.session.save(() => {
+//       response.redirect(`/`);
+//     });
+//   } else {
+//     response.send("??");
+//   }
+// });
 
 router.get("/logout", (request, response) => {
   request.session.destroy((err) => {
